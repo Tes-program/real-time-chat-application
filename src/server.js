12bf49a2276e8144e0713846340215ad412a7492
Tes-program <teslimodumuyiwa@gmail.com>
2023-08-22 09:37:45 +0100

    User Collections Done

--- src/server.js
+++ src/server.js
@@ -1,30 +1,30 @@
-import express from "express";
-import mongoose from "mongoose";
-import * as dotenv from "dotenv";
-import morgan from "morgan";
-import cors from "cors";
-import { userRoute } from "./users/index.js";
+import express from "express"
+import mongoose from "mongoose"
+import * as dotenv from "dotenv"
+import morgan from "morgan"
+import cors from "cors"
+import { userRoute } from "./users/index.js"
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+  console.log(`Server is running on port ${process.env.PORT}`)
+})
 
-app.use(express.json());
-app.use("/user", userRoute);
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use("/user", userRoute)
+app.use(morgan("dev"))
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
 
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
+    useNewUrlParser: true
   })
   .then(() => console.log("MongoDB connected successfully"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err))
 
-export default app;
+export default app
